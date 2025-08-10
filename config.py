752856e636c9a09f8a49f225adec600d80b0b88e
swifted03 <franzcolobong23@gmail.com>
2025-08-10 20:37:47 +0800

    Test config ip address

--- config.py
+++ config.py
@@ -1,9 +1,21 @@
 import socket
 
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # connect to a public IP (does not send packets), to determine outgoing interface IP
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = "127.0.0.1"
+    finally:
+        s.close()
+    return ip
+
+CURRENT_IP = get_local_ip()
 PORT = 50999
 ENCODING = 'utf-8'
 MESSAGE_TERMINATOR = '\n\n'
 BROADCAST_ADDRESS = '<broadcast>'
-CURRENT_IP = socket.gethostbyname(socket.gethostname())
 USERNAME = None
 VERBOSE = False
